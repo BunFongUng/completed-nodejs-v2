d389a414f8e19b7a7bd50c2318e7ef4d4e9f7678
Lester <lester.ung@pathmazing.com>
2017-07-01 22:11:04 +0700

    update

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,23 +1,23 @@
-console.log('app.js started!');
+console.log("app.js started!");
 
-const _ = require('lodash');
-const yargs = require('yargs');
+const _ = require("lodash");
+const yargs = require("yargs");
 
-const notes = require('./notes');
+const notes = require("./notes");
 
 const argv = yargs.argv;
 let command = process.argv[2];
 
 // console.log(argv._);
 
-if (command === 'add') {
-    notes.addNote(argv.title, argv.text);
-} else if (command === 'read') {
-    notes.readNote(argv.title);
-} else if (command === 'list'){
-    notes.listNotes();
-} else if (command === 'delete'){
-    notes.deleteNote(argv.title);
-} else{
-    console.log('command not recognize');
+if (command === "add") {
+  notes.addNote(argv.title, argv.text);
+} else if (command === "read") {
+  notes.readNote(argv.title);
+} else if (command === "list") {
+  notes.listNotes();
+} else if (command === "delete") {
+  notes.deleteNote(argv.title);
+} else {
+  console.log("command not recognize");
 }
