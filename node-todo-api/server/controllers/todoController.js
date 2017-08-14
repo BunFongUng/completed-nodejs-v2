ccf2917156d2d38b613b27129f9ad0159850f442
Lester <lester.ung@pathmazing.com>
2017-08-14 23:24:46 +0700

    add updatetodo in todocontroller

--- node-todo-api/server/controllers/todoController.js
+++ node-todo-api/server/controllers/todoController.js
@@ -1,5 +1,7 @@
 const { ObjectID } = require('mongodb');
 
+const _ = require('lodash');
+
 const { Todo } = require("../models/todo.model");
 
 module.exports = {
@@ -83,6 +85,43 @@ module.exports = {
 			res.status(400).json({
 				status: 'error',
 				error: err
+			});
+		});
+	},
+	updateTodo: (req, res) => {
+		let todoId = req.params.id;
+		let body = _.pick(req.body, ['title', 'completed']);
+
+		if(!ObjectID.isValid(todoId)) {
+			return res.status(404).json({
+				status: 'error',
+				message: 'Invalid todo id'
+			});
+		}
+
+		if(_.isBoolean(body.completed) && body.completed) {
+			body.completedAt = new Date().getTime();
+		} else {
+			body.completed = false;
+			body.completedAt = null;
+		}
+
+		Todo.findByIdAndUpdate(todoId, { $set: body }, { new: true }).then(todo => {
+			if(!todo) {
+				return res.status(404).json({
+					status: 'error',
+					message: 'Todo not found'
+				});
+			}
+
+			res.json({
+				status: 'success',
+				data: todo
+			});
+		}).catch(err => {
+			res.status(400).json({
+				status: 'error',
+				message: err
 			});
 		});
 	}
