8bf98c954007bbad8ad678a92c671fa287ba765a
Lester <lester.ung@pathmazing.com>
2017-08-15 23:35:23 +0700

    add env

--- node-todo-api/server/config/dbConfig.js
+++ node-todo-api/server/config/dbConfig.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
 const { Schema } = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+let env = process.env.NODE_ENV || 'development';
+
+if(env === 'test') {
+	process.env.MONGODB_URL = 'mongodb://localhost:27017/TodosAppV2Test';
+}
+
 try {
-	mongoose.connect('mongodb://localhost:27017/TodosAppV2', {
+	mongoose.connect(process.env.MONGODB_URL, {
   	useMongoClient: true,
 	});
 } catch (error) {
-	mongoose.createConnection('mongodb://localhost:27017/TodosAppV2', {
+	mongoose.createConnection(process.env.MONGODB_URL, {
   	useMongoClient: true,
 	});
 }
